d85c710a3339ae7e2321129a37b0e409e3f19b9f
Yemi Kelani <91276489+yemi-kelani@users.noreply.github.com>
2023-06-02 16:33:00 -0500

    created app skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import * as React from "react";
+
+const Counter = ({ counter, setCounter }) => {
+  return (
+    <div id="counter-page">
+
+      <p>count {counter}</p>
+
+      <button 
+        id="counter-btn"
+        onClick={
+          () => {
+            setCounter(counter + 1);
+          }
+        }>
+        add drink
+      </button>
+
+      <div className="log-options">
+        <button 
+          id="reset-btn"
+          onClick={() => {
+            setCounter(0);
+          }}>
+          reset counter
+        </button>
+        <button 
+          id="save-btn"
+          onClick={() => {
+            // TODO: save count to users records
+
+            // reset count
+            setCounter(0);
+          }}>
+          save and reset
+        </button>
+      </div>
+    </div>
+  )
+}
+
+const History = () => {
+  return (
+    <div id="counter-page">
+      <h3>Alcohol Consumption History</h3>
+      <hr/>
+      history:
+      
+
+      
+    </div>
+  )
+}
+
+const style = {
+  "active": {"": ""}
+}
+ 
+const App = () => {
+  const [page, setPage] = React.useState("counter");
+  const [counter, setCounter] = React.useState(0);
+
+  let renderPage;
+  switch(page) {
+    case "counter":
+      renderPage = <Counter counter={counter} setCounter={setCounter}/>;
+      break;
+    case "history":
+      renderPage = <History/>;
+      break;
+    default:
+      renderPage = <Counter counter={counter} setCounter={setCounter}/>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <nav className="navigation-bar">
+        <ul id="nav-links">
+          <li
+            className="nav-link" 
+            onClick={() => {setPage("counter")}}
+          >Counter</li>
+          <li
+            className="nav-link" 
+            onClick={() => {setPage("history")}}
+          >History</li>
+        </ul>
+      </nav>
+
+      <div className="page-container">
+        {renderPage}
+      </div>
+
     </div>
   );
 }
