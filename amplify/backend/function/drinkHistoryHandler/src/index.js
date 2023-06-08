61fa03b636cac59e992fa3b3985cf4377f70489b
Cheecher1 <willcichowski2023@u.northwestern.edu>
2023-06-08 14:52:53 -0500

    connectd handler to rds

--- amplify/backend/function/drinkHistoryHandler/src/index.js
+++ amplify/backend/function/drinkHistoryHandler/src/index.js
@@ -1,19 +1,48 @@
-
+const mysql = require('mysql');
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(event);
-    const drinks = event.pathParameters.drinks
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-     headers: {
-         "Access-Control-Allow-Origin": "*",
-         "Access-Control-Allow-Headers": "*"
-     },
-        body: JSON.stringify('Added drinks'),
-    };
+    const drinks = event.pathParameters.drinks;
+    const connection = mysql.createConnection({
+        host: 'drinkhistory.ckej3xcnfevi.us-east-2.rds.amazonaws.com', 
+        user: 'admin',
+        password: 'drinktracker',
+        database: 'drinkhistory'
+    });
+
+    connection.connect();
+
+    const queryPromise = new Promise((resolve, reject) => {
+        connection.query('INSERT INTO drinkhistory SET ?', {drink: drinks}, (error, results, fields) => {
+            if (error) {
+                return reject(error);
+            }
+            resolve(results);
+        });
+    });
+
+    let response;
+    try {
+        await queryPromise;
+        response = {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*"
+            },
+            body: JSON.stringify('Added drinks'),
+        };
+    } catch (error) {
+        console.log(error);
+        response = {
+            statusCode: 500,
+            body: JSON.stringify('Failed to insert into the database'),
+        };
+    } finally {
+        connection.end();
+    }
+
     return response;
 };
